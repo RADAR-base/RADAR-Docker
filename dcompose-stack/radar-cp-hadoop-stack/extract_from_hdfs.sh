1ca6a7a8d9776523584eafbe0dca076bc2c3028b
Joris Borgdorff <joris@thehyve.nl>
2017-01-09 15:04:36 +0100

    Tweaks
    - Using hadoop version 2.7.2 everywhere
    - moved network list to the top
    - added tomcat/nodejs/mongo
    - added some more documentation headers

--- dcompose-stack/radar-cp-hadoop-stack/extract_from_hdfs.sh
+++ dcompose-stack/radar-cp-hadoop-stack/extract_from_hdfs.sh
@@ -15,4 +15,4 @@ HDFS_OUTPUT_DIR=/home/output
 HDFS_COMMAND="hdfs dfs -get $HDFS_FILE $HDFS_OUTPUT_DIR"
 
 mkdir -p $OUTPUT_DIR
-docker run --rm --network hadoop -v "$OUTPUT_DIR:$HDFS_OUTPUT_DIR" -e CLUSTER_NAME=radar-cns -e CORE_CONF_fs_defaultFS=hdfs://namenode:8020 uhopper/hadoop $HDFS_COMMAND
+docker run --rm --network hadoop -v "$OUTPUT_DIR:$HDFS_OUTPUT_DIR" -e CLUSTER_NAME=radar-cns -e CORE_CONF_fs_defaultFS=hdfs://namenode:8020 uhopper/hadoop:2.7.2 $HDFS_COMMAND
