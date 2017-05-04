9a5487ba65d84e1c1ffd7ad1db09831327f0c7a4
Joris Borgdorff <joris@thehyve.nl>
2017-05-04 15:58:19 +0200

    Remove git updates about generated code

--- dcompose-stack/radar-cp-hadoop-stack/install-radar-stack.sh
+++ dcompose-stack/radar-cp-hadoop-stack/install-radar-stack.sh
@@ -19,16 +19,27 @@ fi
 echo "==> Setting MongoDB Connector"
 
 # Update sink-mongo.properties
+if [ ! -e etc/sink-mongo.properties ]; then
+  cp etc/sink-mongo.properties.template etc/sink-mongo.properties
+fi
 inline_variable 'mongo.username=' $HOTSTORAGE_USERNAME etc/sink-mongo.properties
 inline_variable 'mongo.password=' $HOTSTORAGE_PASSWORD etc/sink-mongo.properties
 inline_variable 'mongo.database=' $HOTSTORAGE_NAME etc/sink-mongo.properties
-inline_variable 'server_name[[:space:]]*' $SERVER_NAME etc/nginx.conf
 
 # Set topics
 inline_variable 'topics=' "${RADAR_AGG_TOPIC_LIST}" etc/sink-mongo.properties
 
 echo "==> Setting HDFS Connector"
+if [ ! -e etc/sink-hdfs.properties ]; then
+  cp etc/sink-hdfs.properties.template etc/sink-hdfs.properties
+fi
 inline_variable 'topics=' "${RADAR_RAW_TOPIC_LIST}" etc/sink-hdfs.properties
+
+echo "==> Setting nginx"
+if [ ! -e etc/nginx.conf ]; then
+  cp etc/nginx.conf.template etc/nginx.conf
+fi
+inline_variable 'server_name[[:space:]]*' $SERVER_NAME etc/nginx.conf
 
 echo "==> Starting RADAR-CNS Platform"
 sudo-linux docker-compose up --force-recreate -d
