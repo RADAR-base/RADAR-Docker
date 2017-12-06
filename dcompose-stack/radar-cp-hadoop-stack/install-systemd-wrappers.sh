71f54a1499bed0d331dcbea0a3f263ece659fdf1
Joris Borgdorff <joris@thehyve.nl>
2017-12-06 11:05:33 +0100

    Fixed relative execstart in systemd

--- dcompose-stack/radar-cp-hadoop-stack/install-systemd-wrappers.sh
+++ dcompose-stack/radar-cp-hadoop-stack/install-systemd-wrappers.sh
@@ -11,6 +11,8 @@ copy_template_if_absent /etc/systemd/system/radar-output.timer lib/systemd/radar
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 inline_variable 'WorkingDirectory=' "$DIR" /etc/systemd/system/radar-docker.service
+inline_variable 'ExecStart=' "$DIR/lib/systemd/start-radar-stack.sh" /etc/systemd/system/radar-docker.service
+
 inline_variable 'WorkingDirectory=' "$DIR" /etc/systemd/system/radar-output.service
 inline_variable 'ExecStart=' "$DIR/hdfs_restructure.sh /topicAndroidNew output" /etc/systemd/system/radar-output.service
 
