1ffccbfdcc0a886a82eae8dbabd53bb3c7415526
yatharthranjan <yatharthranjan89@gmail.com>
2017-12-06 14:27:49 +0000

    Add renew-certificate systemd service and timer

--- dcompose-stack/radar-cp-hadoop-stack/install-systemd-wrappers.sh
+++ dcompose-stack/radar-cp-hadoop-stack/install-systemd-wrappers.sh
@@ -8,7 +8,9 @@ copy_template_if_absent /etc/systemd/system/radar-docker.service lib/systemd/rad
 copy_template_if_absent /etc/systemd/system/radar-output.service lib/systemd/radar-output.service.template
 copy_template_if_absent /etc/systemd/system/radar-output.timer lib/systemd/radar-output.timer.template
 copy_template_if_absent /etc/systemd/system/radar-check-health.service lib/systemd/radar-check-health.service.template
-copy_template_if_absent /etc/systemd/system/radar-check-health.timer lib/systemd/radar-check-health.timer
+copy_template_if_absent /etc/systemd/system/radar-check-health.timer lib/systemd/radar-check-health.timer.template
+copy_template_if_absent /etc/systemd/system/radar-renew-certificate.service lib/systemd/radar-renew-certificate.service.template
+copy_template_if_absent /etc/systemd/system/radar-renew-certificate.timer lib/systemd/radar-renew-certificate.timer.template
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -21,8 +23,13 @@ inline_variable 'ExecStart=' "$DIR/hdfs_restructure.sh /topicAndroidNew output" 
 inline_variable 'WorkingDirectory=' "$DIR" /etc/systemd/system/radar-check-health.service
 inline_variable 'ExecStart=' "$DIR/check-health.sh" /etc/systemd/system/radar-check-health.service
 
+inline_variable 'WorkingDirectory=' "$DIR" /etc/systemd/system/radar-renew-certificate.service
+inline_variable 'ExecStart=' "$DIR/renew_ssl_certificate.sh" /etc/systemd/system/radar-renew-certificate.service
+
+
 sudo systemctl daemon-reload
 sudo systemctl enable radar-docker
 sudo systemctl enable radar-output.timer
 sudo systemctl enable radar-check-health.timer
+sudo systemctl enable radar-renew-certificate.timer
 sudo systemctl start radar-docker
