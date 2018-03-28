f037ae053083af9cd12768010d5f78d2aae11563
Joris Borgdorff <joris@thehyve.nl>
2018-03-28 13:27:13 +0200

    Some simplification

--- dcompose-stack/radar-cp-hadoop-stack/kafka-radarinit/init.sh
+++ dcompose-stack/radar-cp-hadoop-stack/kafka-radarinit/init.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+AVRO_TOOLS=/usr/share/java/avro-tools.jar
+
 rsync -a /schema/original/commons /schema/original/specifications /schema/merged
 rsync -a /schema/conf/ /schema/merged
 
@@ -13,18 +15,16 @@ echo "Compiling schemas..." >&2
 # a namespace with internal periods.
 DEPENDENT_REGEX='"(items|type)": (\[\s*"null",\s*)?"([A-Z]|[^".]*\.)'
 
-find merged/commons -name "*.avsc" -print | sort > merged/file_list
-find merged/commons -name "*.avsc" -exec grep -Eq "$DEPENDENT_REGEX" "{}" \; -print | sort > merged/file_list_dependent
-comm -23 merged/file_list merged/file_list_dependent > merged/file_list_independent
-
 # Separate enums so that they can be referenced in later files
-read -r -a independent <<< $(tr '\n' ' ' < merged/file_list_independent)
-read -r -a dependent <<< $(tr '\n' ' ' < merged/file_list_dependent)
+find merged/commons -name "*.avsc" -print | sort > merged/file_list
+DEPENDENT=$(find merged/commons -name "*.avsc" -exec grep -Eq "$DEPENDENT_REGEX" "{}" \; -print | sort)
+# remove dependent files from all files to get independent files
+INDEPENDENT=$(printf "${DEPENDENT}" | comm -23 merged/file_list -)
 
-printf "===> Independent schemas:\n$(echo ${independent[@]})\n"
-printf "===> Dependent schemas:\n$(echo ${dependent[@]})\n"
+printf "===> Independent schemas:\n${INDEPENDENT}\n"
+printf "===> Dependent schemas:\n${DEPENDENT}\n"
 
-java -jar /usr/share/java/avro-tools.jar compile -string schema ${independent[@]} ${dependent[@]} java/src 2>/dev/null
+java -jar "${AVRO_TOOLS}" compile -string schema ${INDEPENDENT} ${DEPENDENT} java/src 2>/dev/null
 find java/src -name "*.java" -print0 | xargs -0 javac -cp /usr/lib/*:java/classes -d java/classes -sourcepath java/src 
 # Update the radar schemas so the tools find the new classes in classpath
 jar uf /usr/lib/radar-schemas-commons-${RADAR_SCHEMAS_VERSION}.jar -C java/classes .
