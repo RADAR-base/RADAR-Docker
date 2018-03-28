f40579e0835eec660d1f93b89fb5de24b4400d75
Joris Borgdorff <joris@thehyve.nl>
2018-03-28 12:51:12 +0200

    Ensure that kafka init identifies dependent schemas (fixes #116)

--- dcompose-stack/radar-cp-hadoop-stack/kafka-radarinit/init.sh
+++ dcompose-stack/radar-cp-hadoop-stack/kafka-radarinit/init.sh
@@ -12,10 +12,19 @@ echo "Compiling schemas..." >&2
 # e.g., a literal class starting with a capital, or
 # a namespace with internal periods.
 DEPENDENT_REGEX='"(items|type)": (\[\s*"null",\s*)?"([A-Z]|[^".]*\.)'
+
+find merged/commons -name "*.avsc" -print | sort > merged/file_list
+find merged/commons -name "*.avsc" -exec grep -Eq "$DEPENDENT_REGEX" "{}" \; -print | sort > merged/file_list_dependent
+comm -23 merged/file_list merged/file_list_dependent > merged/file_list_independent
+
 # Separate enums so that they can be referenced in later files
-read -r -a notdependent <<< $(find merged/commons -name "*.avsc" -exec grep -Eqv "$DEPENDENT_REGEX" "{}" \; -print | tr '\n' ' ')
-read -r -a dependent <<< $(find merged/commons -name "*.avsc" -exec grep -Eq "$DEPENDENT_REGEX" "{}" \; -print | tr '\n' ' ')
-java -jar /usr/share/java/avro-tools.jar compile -string schema ${notdependent[@]} ${dependent[@]} java/src 2>/dev/null
+read -r -a independent <<< $(tr '\n' ' ' < merged/file_list_independent)
+read -r -a dependent <<< $(tr '\n' ' ' < merged/file_list_dependent)
+
+printf "===> Independent schemas:\n$(echo ${independent[@]})\n"
+printf "===> Dependent schemas:\n$(echo ${dependent[@]})\n"
+
+java -jar /usr/share/java/avro-tools.jar compile -string schema ${independent[@]} ${dependent[@]} java/src 2>/dev/null
 find java/src -name "*.java" -print0 | xargs -0 javac -cp /usr/lib/*:java/classes -d java/classes -sourcepath java/src 
 # Update the radar schemas so the tools find the new classes in classpath
 jar uf /usr/lib/radar-schemas-commons-${RADAR_SCHEMAS_VERSION}.jar -C java/classes .
