c59d61b94ee058f3ad00d77447a68447426493be
nivethika <nivemaham@gmail.com>
2018-07-05 14:35:00 +0200

    update postgres version with an env variable

--- dcompose-stack/radar-cp-hadoop-stack/migrate-postgres-data.sh
+++ dcompose-stack/radar-cp-hadoop-stack/migrate-postgres-data.sh
@@ -2,20 +2,18 @@
 
 set -e
 
-NEW_VERSION=10.4-alpine
-
 
 . ./.env
 
-POSTGRES_NEW_DIR="${MP_POSTGRES_DIR}/data-${NEW_VERSION}"
+POSTGRES_NEW_DIR="${MP_POSTGRES_DIR}/data-${POSTGRES_NEW_VERSION}"
 
-echo "Migrating ManagementPortal database to ${NEW_VERSION}"
+echo "Migrating ManagementPortal database to ${POSTGRES_NEW_VERSION}"
 if [ -e "${POSTGRES_NEW_DIR}" ]; then
   echo "Please remove old temporary directory $POSTGRES_NEW_DIR before proceeding"
   exit 1
 fi
 
-POSTGRES_NEW=$(docker run -d -v "${POSTGRES_NEW_DIR}/:/var/lib/postgresql/data" --env-file ./.env postgres:"${NEW_VERSION}")
+POSTGRES_NEW=$(docker run -d -v "${POSTGRES_NEW_DIR}/:/var/lib/postgresql/data" --env-file ./.env postgres:"${POSTGRES_NEW_VERSION}")
 sleep 5
 
 docker-compose exec managementportal-postgresql pg_dumpall -U "${POSTGRES_USER}" \
@@ -32,7 +30,7 @@ mv "${MP_POSTGRES_DIR}/data/" "${MP_POSTGRES_DIR}/data-backup-$(date +%FT%TZ)/"
 mv "${POSTGRES_NEW_DIR}" "${MP_POSTGRES_DIR}/data/"
 
 # change postgres version
-sed -i "s| image: postgres:.*| image: postgres:${NEW_VERSION}|" docker-compose.yml
+sed -i "s| image: postgres:.*| image: postgres:${POSTGRES_NEW_VERSION}|" docker-compose.yml
 
 echo "Starting postgres..."
 
