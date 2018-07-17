f296249e5b0cd87b843b5aaf849729d2319d481c
nivethika <nivemaham@gmail.com>
2018-07-17 13:56:26 +0200

    add mock redcap-integration config

--- dcompose-stack/radar-cp-hadoop-stack/travis/setup-env.sh
+++ dcompose-stack/radar-cp-hadoop-stack/travis/setup-env.sh
@@ -15,5 +15,6 @@ cp ../etc/webserver/nginx.conf.template ../etc/webserver/nginx.conf
 cp ../etc/hdfs-connector/sink-hdfs.properties.template ../etc/hdfs-connector/sink-hdfs.properties
 cp ../etc/mongodb-connector/sink-mongo.properties.template ../etc/mongodb-connector/sink-mongo.properties
 cp ../etc/managementportal/config/oauth_client_details.csv.template ../etc/managementportal/config/oauth_client_details.csv
+cp ../etc/redcap-integration/radar.yml.template ../etc/redcap-integration/radar.yml
 
 echo "Setup mock configurations are finished..."
