17b19efa53e29822c587ce78e43deebb54e621c8
nivethika <nivemaham@gmail.com>
2018-07-17 13:50:45 +0200

    mock config setup for travis and use install-radar-stack

---
+++ dcompose-stack/radar-cp-hadoop-stack/travis/setup-env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# create hadoop netword
+sudo docker network create hadoop
+
+# create volumes
+sudo docker volume create certs
+sudo docker volume create certs-data
+
+# setup mock configs
+cp ./travis-env.template ../.env
+cp ./travis-smtp.template ../etc/smtp.env
+cp ../etc/radar-backend/radar.yml.template ../etc/radar-backend/radar.yml
+cp ../etc/webserver/nginx.conf.template ../etc/webserver/nginx.conf
+cp ../etc/hdfs-connector/sink-hdfs.properties.template ../etc/hdfs-connector/sink-hdfs.properties
+cp ../etc/mongodb-connector/sink-mongo.properties.template ../etc/mongodb-connector/sink-mongo.properties
+cp ../etc/managementportal/config/oauth_client_details.csv.template ../etc/managementportal/config/oauth_client_details.csv
+
+echo "Setup mock configurations are finished..."
