a9b39c03f2b5e8dc85f241d5488983690c5bbc96
Joris Borgdorff <github@blootsvoets.net>
2018-11-19 10:16:10 +0100

    Forward-compatible with 10.6-alpine image

--- dcompose-stack/radar-cp-hadoop-stack/images/postgres/multi-db-init.sh
+++ dcompose-stack/radar-cp-hadoop-stack/images/postgres/multi-db-init.sh
@@ -4,15 +4,15 @@ set -u
 
 function create_user_and_database() {
   local database=$1
-  local database_exist=$(psql -tAc "SELECT 1 FROM pg_database WHERE datname='$database'")
+  local database_exist=$(psql -U "$POSTGRES_USER" -tAc "SELECT 1 FROM pg_database WHERE datname='$database';")
   if [[ "$database_exist" == 1 ]]; then
     echo "Database $database already exists"
   else
     echo "Database $database does not exist"
     echo "  Creating database '$database' for user '$POSTGRES_USER'"
-    psql -v ON_ERROR_STOP=1  <<-EOSQL
-    CREATE DATABASE $database;
-    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+    psql -U "$POSTGRES_USER" -v ON_ERROR_STOP=1  <<-EOSQL
+    CREATE DATABASE "$database";
+    GRANT ALL PRIVILEGES ON DATABASE $database TO "$POSTGRES_USER";
 EOSQL
   fi
 }
@@ -23,6 +23,5 @@ if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
   for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
     create_user_and_database $db
   done
-  echo -n "$POSTGRES_MULTIPLE_DATABASES" > /var/run/postgresql/multiple_databases_ready
   echo "Databases created"
 fi
