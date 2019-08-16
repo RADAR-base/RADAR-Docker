8026214dc97d8cd229b25b85562e5da8e8ee5558
yatharthranjan <yatharthranjan89@gmail.com>
2019-08-16 16:09:50 +0100

    Fix hdfs datanode install script

--- distributed-dcompose-stack/hdfs-datanode/bin/install.sh
+++ distributed-dcompose-stack/hdfs-datanode/bin/install.sh
@@ -2,10 +2,4 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-echo "Please select the Node you want to install in this instance."
-select node in "DATA_NODE" "NAME_NODE"; do
-  case ${node} in
-    "DATA_NODE")
-      echo "Please select the Node you want to install in this instance."
-      select number in "1" "2" "3"; do
-        .lib/perform-install.sh "DATA" ${number}
+.lib/perform-install.sh
