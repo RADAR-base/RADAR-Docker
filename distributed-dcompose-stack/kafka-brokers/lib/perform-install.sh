0c5e2f48ca3715d870a7cb2f63aac2198c546661
yatharthranjan <yatharthranjan89@gmail.com>
2019-08-16 17:48:37 +0100

    Upgrade confluent to 5.2.0

--- distributed-dcompose-stack/kafka-brokers/lib/perform-install.sh
+++ distributed-dcompose-stack/kafka-brokers/lib/perform-install.sh
@@ -2,8 +2,10 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-. ../../commons/lib/util.sh
+. ../commons/lib/util.sh
 
 echo "OS version: $(uname -a)"
 check_command_exists docker
 check_command_exists docker-compose
+
+sudo-linux docker-commpose up -d
