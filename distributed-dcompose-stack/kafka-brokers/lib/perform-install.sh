8ee7869596982499b6ccc9b6726bbef0f8878f19
yatharthranjan <yatharthranjan89@gmail.com>
2019-08-19 17:07:25 +0100

    import env

--- distributed-dcompose-stack/kafka-brokers/lib/perform-install.sh
+++ distributed-dcompose-stack/kafka-brokers/lib/perform-install.sh
@@ -8,6 +8,8 @@ echo "OS version: $(uname -a)"
 check_command_exists docker
 check_command_exists docker-compose
 
+. ./.env
+
 ensure_env_password KAFKA_1_HOST "Kafka 1 host is not set .env."
 ensure_env_password KAFKA_2_HOST "Kafka 2 host is not set .env."
 ensure_env_password KAFKA_3_HOST "Kafka 3 host is not set .env."
