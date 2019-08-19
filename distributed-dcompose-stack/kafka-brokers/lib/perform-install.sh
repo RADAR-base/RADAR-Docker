8033d2dc2190df066835dc631d2d176bb608d503
yatharthranjan <yatharthranjan89@gmail.com>
2019-08-19 18:36:55 +0100

    Fix variable checking

--- distributed-dcompose-stack/kafka-brokers/lib/perform-install.sh
+++ distributed-dcompose-stack/kafka-brokers/lib/perform-install.sh
@@ -10,8 +10,6 @@ check_command_exists docker-compose
 
 . ./.env
 
-ensure_env_password KAFKA_1_HOST "Kafka 1 host is not set .env."
-ensure_env_password KAFKA_2_HOST "Kafka 2 host is not set .env."
-ensure_env_password KAFKA_3_HOST "Kafka 3 host is not set .env."
+ensure_env_password HOSTNAME "Host Name is not set .env."
 
 sudo-linux docker-compose up -d
