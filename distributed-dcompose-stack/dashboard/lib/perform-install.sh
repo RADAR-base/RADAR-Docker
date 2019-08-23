be7d3d7b124607d99de281ede195a127f3abc02f
yatharthranjan <yatharthranjan89@gmail.com>
2019-08-23 16:12:37 +0100

    Add dashboard services - rest-api, dashboard, hotstorage and backend-streams

--- distributed-dcompose-stack/dashboard/lib/perform-install.sh
+++ distributed-dcompose-stack/dashboard/lib/perform-install.sh
@@ -2,8 +2,35 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-. ../../commons/lib/util.sh
+. ../commons/lib/util.sh
 
 echo "OS version: $(uname -a)"
 check_command_exists docker
 check_command_exists docker-compose
+
+
+# Initialize and check all config files
+check_config_present .env etc/env.template
+check_config_present etc/radar-backend/radar.yml
+copy_template_if_absent etc/rest-api/radar.yml
+
+. ./.env
+
+check_parent_exists MONGODB_DIR ${MONGODB_DIR}
+
+# Checking provided passwords and environment variables
+ensure_env_default SERVER_NAME localhost
+
+ensure_env_default HOTSTORAGE_USERNAME hotstorage
+ensure_env_password HOTSTORAGE_PASSWORD "Hot storage (MongoDB) password not set in .env."
+ensure_env_default HOTSTORAGE_NAME hotstorage
+
+
+echo "==> Configuring REST-API"
+
+# Set MongoDb credential
+inline_variable 'username:[[:space:]]' "$HOTSTORAGE_USERNAME" etc/rest-api/radar.yml
+inline_variable 'password:[[:space:]]' "$HOTSTORAGE_PASSWORD" etc/rest-api/radar.yml
+inline_variable 'database_name:[[:space:]]' "$HOTSTORAGE_NAME" etc/rest-api/radar.yml
+
+sudo-linux docker-compose up -d
