97878ca11b9fbfd28e823e1bfd541d1ed193687f
yatharthranjan <yatharthranjan89@gmail.com>
2019-09-18 16:41:55 +0100

    add netdata slave to data component

--- distributed-dcompose-stack/commons/bin/install-systemd-wrappers.sh
+++ distributed-dcompose-stack/commons/bin/install-systemd-wrappers.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 echo $(pwd)
---
+++ distributed-dcompose-stack/data/bin/install-netdata-slave.sh
@@ -0,0 +1,20 @@
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+. ../commons/lib/util.sh
+
+echo "OS version: $(uname -a)"
+check_command_exists docker
+check_command_exists docker-compose
+
+echo "==> Configuring Netdata Host monitoring"
+if [[ -n "${NETDATA_MASTER_HOST}" ]]; then
+  sudo-linux cp -p  "../commons/etc/netdata/slave/stream.conf.template" "etc/netdata/slave/stream.conf"
+  sudo-linux cp -p  "../commons/etc/netdata/slave/netdata.conf.template" "etc/netdata/slave/netdata.conf"
+  inline_variable "destination[[:space:]]=[[:space:]]" "${NETDATA_MASTER_HOST}" "etc/netdata/slave/stream.conf"
+  inline_variable "api[[:space:]]key[[:space:]]=[[:space:]]" "${NETDATA_STREAM_API_KEY}" "etc/netdata/slave/stream.conf"
+else
+  echo "NetData Master not configured. Not setting up host monitoring."
+fi
+
+sudo-linux docker-compose up -d netdata-slave
+
