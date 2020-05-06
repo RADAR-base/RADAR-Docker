b79730062dfc085e1bd41608fd63e8bed9ecb837
nivethika <nivemaham@gmail.com>
2020-05-06 16:09:16 +0200

    add modifications to setup s3

--- dcompose-stack/radar-cp-s3-stack/lib/perform-install.sh
+++ dcompose-stack/radar-cp-s3-stack/lib/perform-install.sh
@@ -14,13 +14,13 @@ check_config_present etc/smtp.env
 check_config_present etc/radar-backend/radar.yml
 copy_template_if_absent etc/managementportal/config/oauth_client_details.csv
 copy_template_if_absent etc/mongodb-connector/sink-mongo.properties
-copy_template_if_absent etc/hdfs-connector/sink-hdfs.properties
+copy_template_if_absent etc/s3-connector/sink-s3.properties
 copy_template_if_absent etc/rest-api/radar.yml
 copy_template_if_absent etc/webserver/ip-access-control.conf
 copy_template_if_absent etc/webserver/optional-services.conf
 copy_template_if_absent etc/fitbit/docker/source-fitbit.properties
 copy_template_if_absent etc/rest-source-authorizer/rest_source_clients_configs.yml
-copy_template_if_absent etc/hdfs-restructure/restructure.yml
+copy_template_if_absent etc/output-restructure/restructure.yml
 
 # Set permissions
 sudo-linux chmod og-rw ./.env
@@ -50,13 +50,17 @@ else
 fi
 
 # Check provided directories and configurations
-check_parent_exists HDFS_DATA_DIR_1 ${HDFS_DATA_DIR_1}
-check_parent_exists HDFS_DATA_DIR_2 ${HDFS_DATA_DIR_2}
-check_parent_exists HDFS_DATA_DIR_3 ${HDFS_DATA_DIR_3}
-check_parent_exists HDFS_NAME_DIR_1 ${HDFS_NAME_DIR_1}
-check_parent_exists HDFS_NAME_DIR_2 ${HDFS_NAME_DIR_2}
+check_parent_exists MINIO1_DATA1 ${MINIO1_DATA1}
+check_parent_exists MINIO1_DATA2 ${MINIO1_DATA2}
+check_parent_exists MINIO2_DATA1 ${MINIO2_DATA1}
+check_parent_exists MINIO2_DATA2 ${MINIO2_DATA2}
+check_parent_exists MINIO3_DATA1 ${MINIO3_DATA1}
+check_parent_exists MINIO3_DATA2 ${MINIO3_DATA2}
+check_parent_exists MINIO4_DATA1 ${MINIO4_DATA1}
+check_parent_exists MINIO4_DATA2 ${MINIO4_DATA2}
 check_parent_exists MONGODB_DIR ${MONGODB_DIR}
 check_parent_exists MP_POSTGRES_DIR ${MP_POSTGRES_DIR}
+check_parent_exists UPLOAD_POSTGRES_DIR ${UPLOAD_POSTGRES_DIR}
 
 # Checking provided passwords and environment variables
 ensure_env_default SERVER_NAME localhost
@@ -69,24 +73,16 @@ ensure_env_password POSTGRES_PASSWORD "PostgreSQL password not set in .env."
 ensure_env_default KAFKA_MANAGER_USERNAME kafkamanager-user
 ensure_env_password KAFKA_MANAGER_PASSWORD "Kafka Manager password not set in .env."
 
+ensure_env_default MINIO_ACCESS_KEY radarbase-minio
+ensure_env_default MINIO_INTERMEDIATE_BUCKET_NAME hotstorage
+ensure_env_password MINIO_SECRET_KEY
+
 if [ -z ${PORTAINER_PASSWORD_HASH} ]; then
   query_password PORTAINER_PASSWORD "Portainer password not set in .env."
   PORTAINER_PASSWORD_HASH=$(sudo-linux docker run --rm httpd:2.4-alpine htpasswd -nbB admin "${PORTAINER_PASSWORD}" | cut -d ":" -f 2)
   ensure_variable 'PORTAINER_PASSWORD_HASH=' "${PORTAINER_PASSWORD_HASH}" .env
 fi
 
-# Create networks and volumes
-if ! sudo-linux docker network ls --format '{{.Name}}' | grep -q "^hadoop$"; then
-  echo "==> Creating docker network - hadoop"
-  sudo-linux docker network create --internal hadoop > /dev/null
-elif [ $(docker network inspect hadoop --format "{{.Internal}}") != "true" ]; then
-  echo "==> Re-creating docker network - hadoop"
-  sudo-linux bin/radar-docker quit radar-hdfs-connector hdfs-namenode-1 hdfs-datanode-1 hdfs-datanode-2 hdfs-datanode-3 > /dev/null
-  sudo-linux docker network rm hadoop > /dev/null
-  sudo-linux docker network create --internal hadoop > /dev/null
-else
-  echo "==> Creating docker network - hadoop ALREADY EXISTS"
-fi
 
 echo "==> Checking docker external volumes"
 if ! sudo-linux docker volume ls -q | grep -q "^certs$"; then
@@ -103,6 +99,11 @@ sudo-linux bin/radar-docker run --rm kafka-init
 KAFKA_SCHEMA_RETENTION_MS=${KAFKA_SCHEMA_RETENTION_MS:-5400000000}
 KAFKA_SCHEMA_RETENTION_CMD='kafka-configs --zookeeper "${KAFKA_ZOOKEEPER_CONNECT}" --entity-type topics --entity-name _schemas --alter --add-config min.compaction.lag.ms='${KAFKA_SCHEMA_RETENTION_MS}',cleanup.policy=compact'
 sudo-linux bin/radar-docker exec -T kafka-1 bash -c "$KAFKA_SCHEMA_RETENTION_CMD"
+
+# Initializing minio
+echo "==> Setting up minio cluster and bucket"
+sudo-linux bin/radar-docker up -d minio1 minio2 minio3 minio4
+sudo-linux bin/radar-docker run --rm mc
 
 echo "==> Configuring MongoDB Connector"
 # Update sink-mongo.properties
@@ -124,14 +125,25 @@ if [ -z "${COMBINED_AGG_TOPIC_LIST}"]; then
 fi
 ensure_variable 'topics=' "${COMBINED_AGG_TOPIC_LIST}" etc/mongodb-connector/sink-mongo.properties
 
-echo "==> Configuring HDFS Connector"
+echo "==> Configuring S3 Sink Connector"
 if [ -z "${COMBINED_RAW_TOPIC_LIST}"]; then
   COMBINED_RAW_TOPIC_LIST=$(sudo-linux docker run "${KAFKA_INIT_OPTS[@]}" list_raw.sh 2>/dev/null | tail -n 1)
   if [ -n "${RADAR_RAW_TOPIC_LIST}" ]; then
     COMBINED_RAW_TOPIC_LIST="${RADAR_RAW_TOPIC_LIST},${COMBINED_RAW_TOPIC_LIST}"
   fi
 fi
-ensure_variable 'topics=' "${COMBINED_RAW_TOPIC_LIST}" etc/hdfs-connector/sink-hdfs.properties
+ensure_variable 'topics=' "${COMBINED_RAW_TOPIC_LIST}" etc/s3-connector/sink-s3.properties
+ensure_variable 's3.bucket.name=' "${MINIO_INTERMEDIATE_BUCKET_NAME}" etc/s3-connector/sink-s3.properties
+
+echo "==> Configuring S3 Sink Connector"
+if [ -z "${COMBINED_RAW_TOPIC_LIST}"]; then
+  COMBINED_RAW_TOPIC_LIST=$(sudo-linux docker run "${KAFKA_INIT_OPTS[@]}" list_raw.sh 2>/dev/null | tail -n 1)
+  if [ -n "${RADAR_RAW_TOPIC_LIST}" ]; then
+    COMBINED_RAW_TOPIC_LIST="${RADAR_RAW_TOPIC_LIST},${COMBINED_RAW_TOPIC_LIST}"
+  fi
+fi
+ensure_variable 'topics=' "${COMBINED_RAW_TOPIC_LIST}" etc/s3-connector/sink-s3.properties
+ensure_variable 's3.bucket.name=' "${MINIO_INTERMEDIATE_BUCKET_NAME}" etc/s3-connector/sink-s3.properties
 
 echo "==> Configuring Management Portal"
 sudo-linux bin/radar-docker build --no-cache radarbase-postgresql
