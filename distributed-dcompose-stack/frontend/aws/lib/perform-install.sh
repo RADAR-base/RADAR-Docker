12b42c918c4a6dd154b5808ea2843eb0fdbc9ef0
yatharthranjan <yatharthranjan89@gmail.com>
2020-06-16 23:02:17 +0100

    fix netdata config path

--- distributed-dcompose-stack/frontend/aws/lib/perform-install.sh
+++ distributed-dcompose-stack/frontend/aws/lib/perform-install.sh
@@ -39,8 +39,8 @@ inline_variable 'client_secret:[[:space:]]' "$FITBIT_API_CLIENT_SECRET" etc/rest
 
 echo "==> Configuring Netdata Host monitoring"
 if [[ -n "${NETDATA_MASTER_HOST}" ]]; then
-  cp "../commons/etc/netdata/slave/stream.conf.template" "etc/netdata/slave/stream.conf"
-  cp "../commons/etc/netdata/slave/netdata.conf.template" "etc/netdata/slave/netdata.conf"
+  cp "../../commons/etc/netdata/slave/stream.conf.template" "etc/netdata/slave/stream.conf"
+  cp "../../commons/etc/netdata/slave/netdata.conf.template" "etc/netdata/slave/netdata.conf"
   inline_variable "destination[[:space:]]=[[:space:]]" "${NETDATA_MASTER_HOST}" "etc/netdata/slave/stream.conf"
   inline_variable "api[[:space:]]key[[:space:]]=[[:space:]]" "${NETDATA_STREAM_API_KEY}" "etc/netdata/slave/stream.conf"
 else
