b1d8770bbb40d8371adad67fe12315518ae453f2
Heet Sankesara <heetsankesara3@gmail.com>
2021-01-26 12:55:56 +0000

    Added docker compose for mlflow tracking

---
+++ images/mlflow-server/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# uncomment below when you see any issue with db...
+mlflow db upgrade $DB_URI
+
+echo $DB_URI
+mlflow server \
+    --backend-store-uri $DB_URI \
+    --default-artifact-root $ARTIFACT_STORE \
+    --host $SERVER_HOST \
+    --port $SERVER_PORT
