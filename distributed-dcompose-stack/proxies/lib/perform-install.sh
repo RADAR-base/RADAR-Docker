a0baef7a8f0057310b50f7ea08000bee0222b1d3
yatharthranjan <yatharthranjan89@gmail.com>
2021-03-01 10:20:24 +0000

    Expose push endpoint in the webserver

--- distributed-dcompose-stack/proxies/lib/perform-install.sh
+++ distributed-dcompose-stack/proxies/lib/perform-install.sh
@@ -71,6 +71,8 @@ sed_i 's|\${REDCAP_INTEGRATION_APP_URL}|'"${REDCAP_INTEGRATION_APP_URL}"'|' etc/
 sed_i 's|\${NETDATA_MASTER_HOST}|'"${NETDATA_MASTER_HOST}"'|' etc/webserver/nginx.conf
 sed_i 's|\${HDFS_NAMENODE_UI_URL}|'"${HDFS_NAMENODE_UI_URL}"'|' etc/webserver/nginx.conf
 sed_i 's|\${APPSERVER_ALPHA_URL}|'"${APPSERVER_ALPHA_URL}"'|' etc/webserver/nginx.conf
+sed_i 's|\${PUSH_ENDPOINT_URL}|'"${PUSH_ENDPOINT_URL}"'|' etc/webserver/nginx.conf
+
 
 # Split into IP and port if exists
 NETDATA_MASTER_HOST_SPLIT=($(echo ${NETDATA_MASTER_HOST} | tr ":" "\n"))
