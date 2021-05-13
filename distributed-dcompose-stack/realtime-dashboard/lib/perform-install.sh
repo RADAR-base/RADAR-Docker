e0686bc5a989804407ed638f63d46e95b4b573e3
mpgxvii <mpgxvii@gmail.com>
2021-05-13 23:25:33 +0800

    Fix env vars and install script

--- distributed-dcompose-stack/realtime-dashboard/lib/perform-install.sh
+++ distributed-dcompose-stack/realtime-dashboard/lib/perform-install.sh
@@ -18,7 +18,4 @@ ensure_variable 'connection.url=' "jdbc:postgresql://timescaledb:5432/$TIMESCALE
 ensure_variable 'connection.password=' $TIMESCALEDB_PASSWORD etc/jdbc-connector/sink-timescale.properties
 ensure_variable 'topics=' $DASHBOARD_TOPIC_LIST etc/jdbc-connector/sink-timescale.properties
 
-KAFKA_INIT_DOCKER_PATH=../../dcompose-stack/radar-cp-hadoop-stack/images/radar-kafka-init
-sudo-linux docker build ${KAFKA_INIT_DOCKER_PATH} -t radarbase/kafka-init:${RADAR_SCHEMAS_VERSION}
-
 sudo-linux docker-compose up -d
