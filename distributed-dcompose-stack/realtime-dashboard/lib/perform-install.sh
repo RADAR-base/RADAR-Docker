05dcc1d04ffbe58b44579dcc0edc63fd436e8d68
yatharthranjan <yatharthranjan89@gmail.com>
2021-05-18 11:43:50 +0100

    Add netdata monitoring to realtime dashboard

--- distributed-dcompose-stack/realtime-dashboard/lib/perform-install.sh
+++ distributed-dcompose-stack/realtime-dashboard/lib/perform-install.sh
@@ -18,4 +18,14 @@ ensure_variable 'connection.url=' "jdbc:postgresql://timescaledb:5432/$TIMESCALE
 ensure_variable 'connection.password=' $TIMESCALEDB_PASSWORD etc/jdbc-connector/sink-timescale.properties
 ensure_variable 'topics=' $DASHBOARD_TOPIC_LIST etc/jdbc-connector/sink-timescale.properties
 
+echo "==> Configuring Netdata Host monitoring"
+if [[ -n "${NETDATA_MASTER_HOST}" ]]; then
+  cp "../commons/etc/netdata/slave/stream.conf.template" "etc/netdata/slave/stream.conf"
+  cp "../commons/etc/netdata/slave/netdata.conf.template" "etc/netdata/slave/netdata.conf"
+  inline_variable "destination[[:space:]]=[[:space:]]" "${NETDATA_MASTER_HOST}" "etc/netdata/slave/stream.conf"
+  inline_variable "api[[:space:]]key[[:space:]]=[[:space:]]" "${NETDATA_STREAM_API_KEY}" "etc/netdata/slave/stream.conf"
+else
+  echo "NetData Master not configured. Not setting up host monitoring."
+fi
+
 sudo-linux docker-compose up -d
