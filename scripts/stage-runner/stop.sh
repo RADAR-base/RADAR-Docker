2ab77f77b8359e7fb642081da7e2079b17d9ea8f
Xi Bai <82581439+baixiac@users.noreply.github.com>
2021-07-15 17:52:04 +0100

    Fix health check on gateway, configuration on radar-output-restructure and add more healthcheck/restart (#264)

--- scripts/stage-runner/stop.sh
+++ scripts/stage-runner/stop.sh
@@ -15,3 +15,14 @@ systemctl stop nginx
 systemctl disable nginx.service
 systemctl daemon-reload
 apt-get -y remove nginx nginx-common
+
+# Configure container logs
+cat <<EOF > /etc/docker/daemon.json
+{
+    "log-opts": {
+        "max-size": "30m",
+        "max-file": "7"
+    }
+}
+EOF
+systemctl restart docker
