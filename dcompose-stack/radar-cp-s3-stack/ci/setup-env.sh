cbec58b642d7d47912f692f1edbd2b22394ade43
Xi Bai <baixiac@gmail.com>
2021-07-21 13:52:56 +0100

    fix setup-env for radar-cp-s3-stack

--- dcompose-stack/radar-cp-s3-stack/ci/setup-env.sh
+++ dcompose-stack/radar-cp-s3-stack/ci/setup-env.sh
@@ -2,11 +2,12 @@
 echo "Starting to configure mock configurations for test"
 
 # create folder for docker volumes
-mkdir -p /home/ci/data
+sudo mkdir -p /home/ci/data
 
 # setup mock configs
 cp ./ci-env.template ../.env
 cp ./ci-smtp.template ../etc/smtp.env
+cp ../etc/radar-backend/radar.yml.template ../etc/radar-backend/radar.yml
 cp ../etc/webserver/nginx.conf.template ../etc/webserver/nginx.conf
 cp ../etc/s3-connector/sink-s3.properties.template ../etc/s3-connector/sink-s3.properties
 cp ../etc/mongodb-connector/sink-mongo.properties.template ../etc/mongodb-connector/sink-mongo.properties
