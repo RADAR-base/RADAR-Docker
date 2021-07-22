18e8d9833b2008ad567df3ab2618287af4ef0e14
Xi Bai <baixiac@gmail.com>
2021-07-22 16:10:47 +0100

    costomise topic init

---
+++ dcompose-stack/radar-cp-s3-stack/etc/schema/topic_init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z NO_VALIDATE ]; then
+  radar-schemas-tools validate merged
+fi
+
+# Create topics
+echo "Creating RADAR-base topics..."
+
+for i in {1..5}; do
+    if radar-schemas-tools create -c "${KAFKA_CONFIG_PATH}" -p $KAFKA_NUM_PARTITIONS -r $KAFKA_NUM_REPLICATION -b $KAFKA_NUM_BROKERS -s "${KAFKA_BOOTSTRAP_SERVERS}" merged; then
+        echo "Created topics at attempt ${i}"
+    else
+        if [ i -eq 5 ]; then
+            echo "FAILED TO CREATE TOPICS"
+            exit 1
+        else
+            echo "FAILED TO CREATE TOPICS ... Retrying"
+        fi
+    fi
+done
+echo "Topics created."
+
+echo "Registering RADAR-base schemas..."
+if ! radar-schemas-tools register --force -u "$SCHEMA_REGISTRY_API_KEY" -p "$SCHEMA_REGISTRY_API_SECRET" "${KAFKA_SCHEMA_REGISTRY}" merged; then
+  echo "FAILED TO REGISTER SCHEMAS"
+  exit 1
+fi
+
+echo "Schemas registered."
+
+echo "*******************************************"
+echo "**  RADAR-base topics and schemas ready   **"
+echo "*******************************************"
